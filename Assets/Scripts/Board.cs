4fa849814951007b09c18fcb6dea916b1ea5eab3
DuongMinhHoang <hoangls1008@gmail.com>
2024-08-31 22:30:14 +0700

    CountingElements
    Counting Elements for Brewing Potions

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    BoardTile[] boardTileArr;
+    Dictionary<Element, int> elementNumber;
+    // Start is called before the first frame update
+    private void Awake() {
+        
+    }
+    void Start()
+    {
+        boardTileArr = new BoardTile[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++) {
+            boardTileArr[i] = transform.GetChild(i).GetComponent<BoardTile>();
+        }
+        elementNumber = new Dictionary<Element, int>() {
+            {Element.None, 0},
+            {Element.Metal, 0},
+            {Element.Water, 0},
+            {Element.Wood, 0},
+            {Element.Fire, 0},
+            {Element.Earth, 0}
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CalculateElement() {
+        foreach (BoardTile tile in boardTileArr) {
+            if (tile.element != Element.None) {
+                Debug.Log(tile.name + " - " + tile.element);
+                elementNumber[tile.element] += tile.value;
+            }
+        }
+        foreach (Element element in elementNumber.Keys) {
+            Debug.Log(element + ": " + elementNumber[element]);
+        }
+        elementNumber[Element.Metal] = 0;
+        elementNumber[Element.Water] = 0;
+        elementNumber[Element.Wood] = 0;
+        elementNumber[Element.Fire] = 0;
+        elementNumber[Element.Earth] = 0;
+    }
+}
--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class BoardTile : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Element element{get; private set;}
+    public Element element {get; private set;}
+    public int value {get; private set;}
     private void Awake() {
         element = Element.None;
     }
     void Start()
     {
-        
+        value = 0;
     }
 
     // Update is called once per frame
@@ -24,18 +25,26 @@ public class BoardTile : MonoBehaviour
     }
     public void ChangeElement(Element el) {
         if (element == Element.None) {
+            value = 1;
             element = el;
             GetComponent<SpriteRenderer>().color = GlobalGameVar.Instance().elementDic[el].color;
         } else {
             if (element == GlobalGameVar.Instance().elementDic[el].plus) {
-
+                value = 2;
             } else if (el == GlobalGameVar.Instance().elementDic[element].plus) {
+                value = 2;
                 element = el;
                 GetComponent<SpriteRenderer>().color = GlobalGameVar.Instance().elementDic[el].color;
             } else if (el == GlobalGameVar.Instance().elementDic[element].minus
-                        || element == GlobalGameVar.Instance().elementDic[el].minus) {
-                element = Element.None;
-                GetComponent<SpriteRenderer>().color = Color.white;
+                || element == GlobalGameVar.Instance().elementDic[el].minus) {
+                if (value == 2) {
+                    value = 1;
+                    GetComponent<SpriteRenderer>().color = GlobalGameVar.Instance().elementDic[element].color;
+                } else {
+                    value = 0;
+                    element = Element.None;
+                    GetComponent<SpriteRenderer>().color = Color.white;
+                }
             }
         }
     }
