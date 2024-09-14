546ffef9628f7eab41a6a2da891a918e65146421
manzik2005 <142279432+manzik2005@users.noreply.github.com>
2024-09-15 00:01:30 +0700

    add QuestLog

---
+++ Assets/Scripts/QuestPrefab.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestPrefab : MonoBehaviour
+{
+    public Button button { get;  set; }
+    public TextMeshProUGUI buttonText;
+    public int questCollected { get;  set; }
+    public int questColletion { get;  set; }
+    public void Initialize(string displayName, int questCollected = 0, int questColletion = 0) 
+    {
+        this.questCollected = questCollected;
+        this.questColletion = questColletion;
+        this.button = this.GetComponent<Button>();
+        this.buttonText = this.GetComponentInChildren<TextMeshProUGUI>();
+
+        this.buttonText.text = displayName;
+    }
+
+}
