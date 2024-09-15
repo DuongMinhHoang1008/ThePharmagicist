0b5b26de86ec697333f6411ddd11599c7fd3378f
nmhung1294 <ngmanhhung.1294@gmail.com>
2024-09-16 01:00:17 +0700

    claim silv-nft

--- Assets/Scripts/BlockchainScripts.cs
+++ Assets/Scripts/BlockchainScripts.cs
@@ -15,7 +15,7 @@ public class BlockchainManager : MonoBehaviour
 {
     public UnityEvent<string> OnLoggedIn;
     public string Address { get; private set; }
-
+    public TransactionResult result{ get; private set; }
     public static BlockchainManager Instance { get; private set; }
     public Button ClaimButton;
     public TextMeshPro goldText;
@@ -40,4 +40,108 @@ public class BlockchainManager : MonoBehaviour
 
         Debug.Log("Clamed Gold: " + result);
     }
+
+    public async void ClaimSilv(int level) { // easy - 1 , medium - 2 , hard - 3
+        Address = await ThirdwebManager.Instance.SDK.Wallet.GetAddress();
+        Debug.Log("Claiming Silver...");
+        var contract = ThirdwebManager.Instance.SDK.GetContract("0x957d31118d993E52B9414856EA4666f11EB79984");
+        if (level == 1)
+        {
+            result = await contract.ERC20.ClaimTo(Address, "10");
+        }
+        else if (level == 2)
+        {
+            result = await contract.ERC20.ClaimTo(Address, "20");
+        }
+        else if (level == 3)
+        {
+            result = await contract.ERC20.ClaimTo(Address, "30");
+        }
+        Debug.Log("Clamed Silver: " + result);
+    }
+    public async void ClaimNFT(string name) {
+        Address = await ThirdwebManager.Instance.SDK.Wallet.GetAddress();
+        var contract = ThirdwebManager.Instance.SDK.GetContract("0xE6FC216dBb76B25af3D3d78643B89474bF645CF8");
+        if (name == "Thuoc_bach_kim_1")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "1", 1);
+        }
+        else if (name == "Thuoc_bach_kim_2")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "2", 1);
+        }
+        else if (name == "Thuoc_bach_kim_3")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "3;", 1);
+        }
+        else if (name == "Thuoc_han_vu_1")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "4;", 1);
+        }
+        else if (name == "Thuoc_han_vu_2")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "5;", 1);
+        }
+        else if (name == "Thuoc_han_vu_3")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "6;", 1);
+        }
+        else if (name == "Thuoc_hoa_diem_1")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "7;", 1);
+        }
+        else if (name == "Thuoc_hoa_diem_2")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "8;", 1);
+        }
+        else if (name == "Thuoc_hoa_diem_3")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "9;", 1);
+        }
+        else if (name == "Thuoc_hoang_ha_1")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "10", 1);
+        }
+        else if (name == "Thuoc_hoang_ha_2")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "11", 1);
+        }
+        else if (name == "Thuoc_hoang_ha_3")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "12", 1);
+        }
+        else if (name == "Thuoc_hong_ha_1")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "13", 1);
+        }
+        else if (name == "Thuoc_hong_ha_2")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "14", 1);
+        }
+        else if (name == "Thuoc_hong_ha_3")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "15", 1);
+        }
+        else if (name == "Thuy_cau")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "16", 1);
+        }
+        else if (name == "Cau_lua")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "20", 1);
+        }
+        else if (name == "Gai_doc")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "16", 1);
+        }
+        else if (name == "Kim_tieu")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "17", 1);
+        }
+        else if (name == "Nam_dam_da")
+        {
+            result = await contract.ERC1155.ClaimTo(Address, "18", 1);
+        }
+    }
+        
 }
