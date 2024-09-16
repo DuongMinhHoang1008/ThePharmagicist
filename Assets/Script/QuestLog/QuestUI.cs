06cd8b6559440dc424b4287f31c0d2f5be980650
manzik2005 <142279432+manzik2005@users.noreply.github.com>
2024-09-16 20:42:44 +0700

    finishedQuestLog

---
+++ Assets/Script/QuestLog/QuestUI.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.ComponentModel;
+
+public class QuestUI : MonoBehaviour
+{
+    [SerializeField] GameObject contentParent;
+    [SerializeField] QuestLogScrollingList questList;
+    [SerializeField] TextMeshProUGUI QuestName;
+    [SerializeField] TextMeshProUGUI QuestDescribe;
+    [SerializeField] TextMeshProUGUI QuestStatus;
+    [SerializeField] TextMeshProUGUI QuestReward;
+    [SerializeField] TextMeshProUGUI TimeRefreshQuest;
+    private float remainTime = 10;
+    private int indexActiveLastest;
+    void Update()
+    {
+        HideAndShowUI();
+        SwitchQuest(questList);
+        remainTime -= Time.deltaTime;
+        transferTime(remainTime);
+
+        if (remainTime <= 0)
+        {
+            questList.isNeedRefresh = true;
+            remainTime = 10;
+        }
+    }
+
+    private void HideAndShowUI()
+    {
+        if (contentParent.activeInHierarchy && Input.GetKeyDown(KeyCode.Q))
+        {
+            contentParent.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            contentParent.SetActive(true);
+        }
+    }
+
+    private void ShowQuestInfo(QuestPrefab quest)
+    {
+        QuestName.text = quest.buttonText.text;
+        if (quest.gameObject.name == "collectHerbal")
+        {
+            QuestName.fontSize = 15;
+            QuestDescribe.text = "Mô tả \nThu thâp đủ thảo dược để hoàn hành nhiệm vụ";
+        }
+        else if (quest.gameObject.name == "savePeople")
+        {
+            QuestName.fontSize = 20;
+            QuestDescribe.text = "Mô tả \nCứu người bị bệnh";
+        }
+
+        QuestStatus.text = "Tiến độ          " + quest.questCollected + " / " + quest.questColletion;
+    }
+
+    private void SwitchQuest(QuestLogScrollingList questList)
+    {
+        bool isSelected = false;
+        if (contentParent.activeInHierarchy)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (questList.questStored[i] != null && questList.questStored[i].gameObject == EventSystem.current.currentSelectedGameObject)
+                {
+                    ShowQuestInfo(questList.questStored[i]);
+                    indexActiveLastest = i;
+                    isSelected = true;
+                }
+            }
+            if (!isSelected)
+            {
+                questList.questStored[indexActiveLastest].button.Select();
+            }
+        }      
+    }
+
+    private void transferTime(float remainTime)
+    {
+        int minutes = Mathf.FloorToInt(remainTime / 60);
+        int seconds = Mathf.FloorToInt(remainTime % 60);
+        TimeRefreshQuest.text = "Làm mới:       " + string.Format("{00:00}:{1:00}", minutes, seconds);
+    }
+}
