cbdc77d087e0a02351e55842c11c40b9045353cd
hoangtuanlsht <hoangtuanlsht@gmail.com>
2024-09-17 21:40:24 +0700

    j

--- Assets/Scripts/Dialog/UseElement.cs
+++ Assets/Scripts/Dialog/UseElement.cs
@@ -1,20 +1,34 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [SerializeField]
 public class UseElement : MonoBehaviour
 {
-    public int[] lackElement;
-    public int[] needElement;
+    [Header("Metal")]
+    [SerializeField] int metalValue = 0;
+    [Header("Water")]
+    [SerializeField] int waterValue = 0;
+    [Header("Wood")]
+    [SerializeField] int woodValue = 0;
+    [Header("Fire")]
+    [SerializeField] int fireValue = 0;
+    [Header("Earth")]
+    [SerializeField] int earthValue = 0;
+    
 
     private static UseElement instance;
-    private void Awake()
-    {
-        lackElement = new int[5];
-        needElement = new int[5];
-    }
     public static UseElement GetInstance()
     {
         return instance;
     }
+    public string GetInfoSick()
+    {
+        string info = "Kim: " + metalValue + "\n"
+                    + "Thủy: " + waterValue + "\n"
+                    + "Mộc: " + woodValue + "\n"
+                    + "Hỏa: " + fireValue + "\n"
+                    + "Thổ: " + earthValue;
+        return info;
+    }
+    
 }
