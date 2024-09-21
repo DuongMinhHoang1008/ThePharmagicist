4824d0d598ac5e6ff8388ff34c34a339074bf0ca
DuongMinhHoang <hoangls1008@gmail.com>
2024-09-21 10:03:30 +0700

    Beta Build

--- Assets/Script/QuestLog/QuestUI.cs
+++ Assets/Script/QuestLog/QuestUI.cs
@@ -85,7 +85,7 @@ public class QuestUI : MonoBehaviour
 
         QuestStatus.text = "Tiến độ  " + quest.questCollected + " / " + quest.questColletion;
         if (quest.questCollected >= quest.questColletion) {
-            QuestStatus.text += "   Đã hoàn thành";
+            QuestStatus.text += " Hoàn thành";
         }
     }
 
